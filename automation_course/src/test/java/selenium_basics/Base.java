2622cb69ce127bdf03a598759d202e787b4654e3
bhagya <bhagyaprasanth@gmail.com>
2025-12-05 10:01:13 +0530

    day1

---
+++ automation_course/src/test/java/selenium_basics/Base.java
@@ -0,0 +1,25 @@
+package selenium_basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base {
+	public WebDriver driver;
+	public void initializeReturn() {
+		//driver=new ChromeDriver();
+		driver=new FirefoxDriver();
+		driver.get("https://selenium.qabible.in/");
+		driver.manage().window().maximize();
+		driver.manage().window().minimize();
+	}
+	public void closeBrowser() {
+		//driver.close();//to close a tab
+		driver.quit();// to close the window
+	}
+	public static void main(String args[]) {
+		Base base=new Base();
+		base.initializeReturn();
+		//base.closeBrowser();
+	}
+}
---
+++ automation_course/src/test/java/selenium_basics/HandleBrowserCommands.java
@@ -0,0 +1,19 @@
+package selenium_basics;
+
+public class HandleBrowserCommands extends Base{
+	
+	public void verifyBrowserCommands() {
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getPageSource());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HandleBrowserCommands handleBrowserCommands=new HandleBrowserCommands();
+		handleBrowserCommands.initializeReturn();
+		handleBrowserCommands.verifyBrowserCommands();
+	}
+
+}
