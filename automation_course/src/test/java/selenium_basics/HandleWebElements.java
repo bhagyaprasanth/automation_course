c881a4f5d223057a3defa6cfe41ce373776eb253
bhagya <bhagyaprasanth@gmail.com>
2025-12-10 08:36:38 +0530

    Day - 4 Handling web elements

---
+++ automation_course/src/test/java/selenium_basics/HandleWebElements.java
@@ -0,0 +1,27 @@
+package selenium_basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandleWebElements extends Base {
+	public void verifyCommands() {
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement textBox = driver.findElement(By.xpath("//input[@id='single-input-field']"));
+		textBox.sendKeys("Hello");
+		WebElement showMsgBtn = driver.findElement(By.xpath("//button[@id='button-one']"));
+		showMsgBtn.click();
+		WebElement msgTxt = driver.findElement(By.xpath("//div[@id='message-one']"));
+		System.out.println(msgTxt.getText());
+		System.out.println(msgTxt.getTagName());
+		System.out.println(msgTxt.getAttribute("id"));
+		textBox.clear();
+	}
+
+	public static void main(String[] args) {
+		HandleWebElements handleWebElements = new HandleWebElements();
+		handleWebElements.initializeReturn();
+		handleWebElements.verifyCommands();
+
+	}
+
+}
---
+++ automation_course/src/test/java/selenium_basics/HandlingDropdowns.java
@@ -0,0 +1,48 @@
+package selenium_basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class HandlingDropdowns extends Base {
+	public void verifyDropdown() {
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement link = driver.findElement(By.linkText("Select Input"));
+		link.click();
+		WebElement dropdown = driver.findElement(By.xpath("//select[@id='single-input-field']"));
+		Select select = new Select(dropdown);
+		// select.selectByIndex(1);
+		// select.selectByValue("Yellow");
+		select.selectByVisibleText("Green");
+	}
+
+	public void verifyCheckBoxes() {
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement link = driver.findElement(By.linkText("Checkbox Demo"));
+		link.click();
+		WebElement checkbox = driver.findElement(By.xpath("//input[@id='gridCheck']"));
+		checkbox.click();
+		System.out.println(checkbox.isSelected());
+		System.out.println(checkbox.isDisplayed());
+		System.out.println(checkbox.isEnabled());
+
+	}
+	public void verifyRadioButton() {
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement link = driver.findElement(By.linkText("Radio Buttons Demo"));
+		link.click();
+		WebElement radioBtn=driver.findElement(By.xpath("//input[@id='inlineRadio1']"));
+		radioBtn.click();
+		WebElement showBtn=driver.findElement(By.xpath("//button[@id='button-one']"));
+		showBtn.click();
+	}
+
+	public static void main(String[] args) {
+		HandlingDropdowns handlingDropdowns = new HandlingDropdowns();
+		handlingDropdowns.initializeReturn();
+		// handlingDropdowns.verifyDropdown();
+		//handlingDropdowns.verifyCheckBoxes();
+		handlingDropdowns.verifyRadioButton();
+	}
+
+}
