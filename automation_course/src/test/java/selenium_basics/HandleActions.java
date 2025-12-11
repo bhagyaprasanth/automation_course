3f39c5db9b9b8cd46affc94d58743e1e1c547afd
bhagya <bhagyaprasanth@gmail.com>
2025-12-11 08:28:19 +0530

    Day 5 - Handling tables

---
+++ automation_course/src/test/java/selenium_basics/HandleActions.java
@@ -0,0 +1,9 @@
+package selenium_basics;
+
+public class HandleActions extends Base {
+
+	public static void main(String[] args) {
+		HandleActions handleActions = new HandleActions();
+	}
+
+}
---
+++ automation_course/src/test/java/selenium_basics/HandlingTables.java
@@ -0,0 +1,31 @@
+package selenium_basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandlingTables extends Base {
+	public void verifyTables() {
+		driver.navigate().to("https://money.rediff.com/indices/nse");
+		WebElement table = driver.findElement(By.xpath("//table[@id='dataTable']"));
+		System.out.println("Contents of the table");
+		System.out.println(table.getText());
+		WebElement tableRow = driver.findElement(By.xpath("//table[@id='dataTable']/tbody/tr[3]"));
+		System.out.println("Contents of 3rd row of the table");
+		System.out.println(tableRow.getText());
+		WebElement tableColumn = driver.findElement(By.xpath("//table[@id='dataTable']/tbody/tr[3]/td[2]"));
+		System.out.println("Contents of 2nd column and 3rd row of the table");
+		System.out.println(tableColumn.getText());
+		WebElement tableLastRow = driver.findElement(By.xpath("//table[@id='dataTable']/tbody/tr[last()]"));
+		System.out.println("Contents of last row of the table");
+		System.out.println(tableLastRow.getText());
+		WebElement tableLastRowlastColumn = driver.findElement(By.xpath("//table[@id='dataTable']/tbody/tr[last()]/td[last()]"));
+		System.out.println("Contents of last row , last column of the table");
+		System.out.println(tableLastRowlastColumn.getText());
+	}
+
+	public static void main(String[] args) {
+		HandlingTables handlingTables = new HandlingTables();
+		handlingTables.initializeReturn();
+		handlingTables.verifyTables();
+	}
+}
