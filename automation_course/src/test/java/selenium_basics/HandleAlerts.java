e0e9a67a7e66207e190bdb55c5e2ecea3bafec72
bhagya <bhagyaprasanth@gmail.com>
2025-12-15 08:04:38 +0530

    Day 6 - handling frames and multiple windows

---
+++ automation_course/src/test/java/selenium_basics/HandleAlerts.java
@@ -0,0 +1,10 @@
+package selenium_basics;
+
+public class HandleAlerts extends Base{
+
+	public static void main(String[] args) {
+		HandleAlerts handleAlerts=new HandleAlerts();
+		handleAlerts.initializeBrowser();
+	}
+
+}
--- automation_course/src/test/java/selenium_basics/HandleFrames.java
+++ automation_course/src/test/java/selenium_basics/HandleFrames.java
@@ -8,16 +8,25 @@ import org.openqa.selenium.WebElement;
 public class HandleFrames extends Base {
 	public void verifyFrames() {
 		driver.navigate().to("https://demoqa.com/frames");
-		List <WebElement> totalFrames = driver.findElements(By.tagName("iframe"));
-		System.out.println("No of Frames in the page " );
+		List<WebElement> totalFrames = driver.findElements(By.tagName("iframe"));
+		System.out.println("No of Frames in the page ");
 		System.out.println(totalFrames.size());
+//		driver.switchTo().frame(1); // switch to frame by index
+//		driver.switchTo().frame("frame1"); //// switch to frame by ID
+		WebElement iframe = driver.findElement(By.xpath("//iframe[@id='frame1']")); 
+		driver.switchTo().frame(iframe); // switch to frame by web element
+//		driver.switchTo().parentFrame();
+		WebElement h1 = driver.findElement(By.xpath("//h1[@id='sampleHeading']"));
+		System.out.println(h1.getText());
+		driver.switchTo().defaultContent();//to release focus from the iframe 
+
 	}
 
 	public static void main(String[] args) {
 		HandleFrames handleFrames = new HandleFrames();
 		handleFrames.initializeBrowser();
-		handleFrames.verifyFrames();	
-		handleFrames.closeBrowser();
+		handleFrames.verifyFrames();
+//		handleFrames.closeBrowser();
 	}
 
 }
