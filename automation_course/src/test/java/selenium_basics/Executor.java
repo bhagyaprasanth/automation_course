5486f5b0f5f202f01d2877bdcf7e481148a2a131
bhagya <bhagyaprasanth@gmail.com>
2025-12-16 08:05:11 +0530

    Day - 7 (Alerts and Scrolling)

---
+++ automation_course/src/test/java/selenium_basics/Executor.java
@@ -0,0 +1,28 @@
+package selenium_basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class Executor extends Base {
+	public void verifyJavascriptExecutor() {
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement showMessageButton = driver.findElement(By.xpath("//button[@id='button-one']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", showMessageButton);
+		js.executeScript("window.scrollBy(0,500)", "");//scrollBy -  relative scrolling
+		// js.executeScript("window.scrollBy(0,0)", "");
+		// //System.out.println(js.executeScript("return window.pageYOffset"));
+		// js.executeScript("window.scrollBy(200,300)", "");
+//		js.executeScript("window.scrollTo(0,0)", "");//scrollTo - absolute scrolling
+		// js.executeScript("window.scrollTo(0, document.body.scrollHeight)", "");
+		// js.executeScript("window.scrollTo(200,300)", "");
+	}
+
+	public static void main(String[] args) {
+		Executor executor=new Executor();
+		executor.initializeBrowser();
+		executor.verifyJavascriptExecutor();
+	}
+
+}
--- automation_course/src/test/java/selenium_basics/HandleAlerts.java
+++ automation_course/src/test/java/selenium_basics/HandleAlerts.java
@@ -1,10 +1,42 @@
 package selenium_basics;
 
-public class HandleAlerts extends Base{
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandleAlerts extends Base {
+
+	public void verifySimpleAlert() {
+		driver.navigate().to("https://demoqa.com/alerts");
+		WebElement alertBtn = driver.findElement(By.xpath("//button[@id='alertButton']"));
+		alertBtn.click();
+		Alert simpleAlert = driver.switchTo().alert();
+		System.out.println(simpleAlert.getText());
+		simpleAlert.accept();//to click ok button
+	}
+	public void verifyConfirmAlert() {
+		driver.navigate().to("https://demoqa.com/alerts");
+		WebElement confirmAlertBtn = driver.findElement(By.xpath("//button[@id='confirmButton']"));
+		confirmAlertBtn.click();
+		Alert confirmAlert = driver.switchTo().alert();
+//		confirmAlert.accept();
+		confirmAlert.dismiss();//to click cancel button
+	}
+	public void verifyPromptAlert() {
+		driver.navigate().to("https://demoqa.com/alerts");
+		WebElement promptAlertBtn = driver.findElement(By.xpath("//button[@id='promtButton']"));
+		promptAlertBtn.click();
+		Alert promptAlert = driver.switchTo().alert();
+		promptAlert.sendKeys("Bhagya");
+		promptAlert.accept();
+	}
 
 	public static void main(String[] args) {
-		HandleAlerts handleAlerts=new HandleAlerts();
+		HandleAlerts handleAlerts = new HandleAlerts();
 		handleAlerts.initializeBrowser();
+		//handleAlerts.verifySimpleAlert();
+//		handleAlerts.verifyConfirmAlert();
+		handleAlerts.verifyPromptAlert();
 	}
 
 }
